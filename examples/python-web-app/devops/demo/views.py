0626ac92fb17bc39cb10e67cdac6aaf6ef8b3eb3
carl300 <124004077+carl300@users.noreply.github.com>
2026-02-14 14:59:36 -0500

    Update views.py

--- examples/python-web-app/devops/demo/views.py
+++ examples/python-web-app/devops/demo/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'demo_site.html')
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello from Django on EC2!")
